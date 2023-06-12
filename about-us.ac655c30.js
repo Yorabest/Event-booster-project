05cbdedae84f38134cfb425963bb08af2cdda78d
Yorabest <105670711+Yorabest@users.noreply.github.com>
2023-06-12 13:22:44 +0000

    Deploying to gh-pages from @ Yorabest/Event-booster-project@fbf8e26c43cf10cb1165c49e69c5ad51c72dae3d 🚀

--- about-us.ac655c30.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("#myModal"),n=document.querySelector("#myBtn"),o=document.querySelector(".close");n.onclick=function(){e.style.display="block"},o.onclick=function(){e.style.display="none"},window.onclick=function(n){n.target===e&&(e.style.display="none")},fetch(/discovery/v2/events/{id:id}).then((e=>e.json())).then((e=>console.log(e)));
-//# sourceMappingURL=about-us.ac655c30.js.map
