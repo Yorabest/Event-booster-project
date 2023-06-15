9b645da5df01c54f2c41c6921c2f58d41735af6f
Mukola <koliunia2006@gmail.com>
2023-06-15 19:09:02 +0300

    error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import './js/about-this-event';
-import './js/about-us';
 import './js/modal';
 import './js/plugination';
 import './js/search-by-country';
 import './js/search-by-key-word';
 import './js/favorite'
 import './js/cards';
+import './js/about-us';
