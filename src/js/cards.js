3bd685758e8fc92d0969f7a4c47cda7f20d54853
Sophia <105670711+Yorabest@users.noreply.github.com>
2023-06-20 07:40:02 +0300

    Revert "error"

--- src/js/cards.js
+++ src/js/cards.js
@@ -1,94 +1,32 @@
-import axios from 'axios';
+import axios from 'axios'
 import Handlebars from 'handlebars';
-import templateSource from './cards.hbs';
 
-const template = Handlebars.compile(templateSource);
-
-async function getAllEvents() {
-  try {
-    const response = await axios.get('https://app.ticketmaster.com/discovery/v2/events.json?apikey=eXb8ULUoq4HjIKYn2xDLaMMehZFueL04');
-    const data = response.data;
-    const events = data._embedded.events;
-    return events;
-  } catch (error) {
-    console.log('Error:', error);
-  }
+function getAllEvents() {
+    axios.get('https://developer.ticketmaster.com/products-and-docs/apis/discovery-api/v2/#supported-country-codes')
+    .then(res=>res.data)
+    .then(data=>data._embedded.events)
+    .then(console.log)
 }
-
-async function renderEvents() {
-  const events = await getAllEvents();
-  events.forEach(event => {
-    const renderedHtml = template(event);
-    document.getElementById('container').innerHTML += renderedHtml;
-  });
-}
-
-window.addEventListener('DOMContentLoaded', renderEvents);
-
-
-
-
-
-
-
-
-
-
-// import axios from 'axios';
-// import Handlebars from 'handlebars';
-// import templateSource from './cards.hbs';
-
-// const template = Handlebars.compile(templateSource);
-
-// async function getAllEvents() {
-//   try {
-//     const response = await axios.get('https://app.ticketmaster.com/discovery/v2/events.json?apikey=eXb8ULUoq4HjIKYn2xDLaMMehZFueL04');
-//     const data = response.data;
-//     const events = data._embedded.events;
-//     renderEvents(events);
-//   } catch (error) {
-//     console.log('Error:', error);
-//   }
-// }
-
-// function renderEvents(events) {
-//   events.forEach(event => {
-//     const renderedHtml = template(event);
-//     document.getElementById('container').innerHTML += renderedHtml;
-//   });
-// }
-
-// getAllEvents();
-
-
-
-
-
-
-
-
-// const templateSource = document.getElementById('card').innerHTML;
-// const template = Handlebars.compile(templateSource);
-
-// import axios from 'axios'
-// import Handlebars from 'handlebars';
-
-// async function getAllEvents() {
-//     try {
-//         const response = await axios.get('https://app.ticketmaster.com/discovery/v2/events.json?apikey=eXb8ULUoq4HjIKYn2xDLaMMehZFueL04')
-//         const data = await response.data
-//         const events = await data._embedded.events
-//         console.log(events);
-//         return events
-//     } catch(error) {
-//         console.log('error');
-//     }
-// }
 
 // getAllEvents()
 
-// getAllEvents().then(events => {
-//     const html = template({ items: events });
-//     const container = document.getElementById('container');
-//     container.innerHTML = html;
-// });
+
+// const container = document.getElementById('container');
+// axios.get('template.hbs')
+//     .then(res => res.data)
+//     .then(templateStr => {
+//         const template = Handlebars.compile(templateStr);
+
+//         axios.get('https://app.ticketmaster.com/discovery/v2/events.json?keyword=devjam&source=universe&countryCode=US&apikey=%7Bapikey%7D')
+//             .then(res => res.data)
+//             .then(data => {
+//                 const events = data._embedded.events.map(event => ({
+//                     ...event,
+//                     imgSrc: event.images[0].url
+//                 }));
+
+//                 const html = template({ items: events });
+
+//                 container.innerHTML = html;
+//             });
+//     });
