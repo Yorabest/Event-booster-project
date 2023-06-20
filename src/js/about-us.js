8e8e035420828b76692b43855fdca17f3e5cc1ac
Sophia <105670711+Yorabest@users.noreply.github.com>
2023-06-20 12:55:57 +0300

    Sri

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -11,7 +11,7 @@ function makeCard(people) {
 
         `<li class='about-us-li'>
              <h3 class='our-h3'>${people.name}</h3>
-             <img class='our-img'>${people.photo}</img>
+             <img class='our-img' src=${people.photo}></img>
              <p class='our-p-work'>Роботу, яку виконував на проекті : ${people.work}</p>
              <p class='our-p-mail'>E-mail : ${people.mail}</p>
              <p class='our-p-git'>Github : ${people.git}</p>
