95b5d6560f80bd0232f0d40ba53f4150a0d2a1cd
Sophia <melnichenko.cofia07@gmail.com>
2023-06-25 09:47:44 +0300

    smth

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -1,21 +1,22 @@
 import json from './our-info.json'
 
-// console.log(json.people[1].mail);
+// console.log(json.people[7].photo);
 // const ourJson = JSON.stringify(json.people)
 // const parse = JSON.parse(ourJson)
 
 const ourCard = document.querySelector('.js-card-container')
 
 function makeCard(people) {
-    const a = people.map(people => 
-
-        `<li class='about-us-li'>
+    
+    const a = people.map(people => {
+        console.log(people.photo)
+       return `<li class='about-us-li'>
              <h3 class='our-h3'>${people.name}</h3>
-             <img class='our-img' src=${people.photo}></img>
+             <img class='our-img' src="${people.photo}"></img>
              <p class='our-p-work'>Роботу, яку виконував на проекті : ${people.work}</p>
              <p class='our-p-mail'>E-mail : ${people.mail}</p>
              <p class='our-p-git'>Github : ${people.git}</p>
-        </li>`).join('')
+        </li>`}).join('')
     return a
 }
 const murkup = makeCard(json.people)
