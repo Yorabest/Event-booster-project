15b40f058550a08e20fb2164b5b2c0b962fa28c5
Sophia <105670711+Yorabest@users.noreply.github.com>
2023-06-26 16:09:25 +0300

    Update about-us.js

--- src/js/about-us.js
+++ src/js/about-us.js
@@ -8,14 +8,14 @@ const ourCard = document.querySelector('.js-card-container')
 
 function makeCard(people) {
 
-    const itemsTeam = people.map(people => 
+    const itemsTeam = people.map(people => {
        return `<li class='about-us-li'>
              <h3 class='our-h3'>${people.name}</h3>
              <img class='our-img' src="${people.photo}"></img>
              <p class='our-p-work'>Роботу, яку виконував на проекті : ${people.work}</p>
              <p class='our-p-mail'>E-mail : ${people.mail}</p>
              <p class='our-p-git'>Github : ${people.git}</p>
-        </li>`).join('')
+        </li>`}).join('')
 }
 const murkup = makeCard(json.people)
 // console.log(murkup);
