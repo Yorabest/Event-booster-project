0af6f38a28ebb84828c6a94eae2239946d714831
Oleh Tsap <olehtsap07@gmail.com>
2023-06-27 19:35:21 +0300

    зробив робочу кнопку

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -1,11 +1,13 @@
 const refs = {
     btnSwitchTheme: document.querySelector('.switch-theme'),
+    switchPage: document.querySelector('.tui-page-btn'),
 }
 
 const bodyElment = document.body
 
 refs.btnSwitchTheme.addEventListener('click', switchTheme);
 
-function switchTheme () {
+function switchTheme() {
     bodyElment.classList.toggle('light-theme');
+    document.refs.switchPage.classList.toggle('switch-page')
 }
