194a23d4d5456c2777291a0bc26c90a0109c5c71
Vlad <vladikhoncharuk@gmail.com>
2023-06-29 18:36:47 +0300

    commit

--- src/js/search-by-country.js
+++ src/js/search-by-country.js
@@ -1,16 +1,22 @@
 import axios from "axios";
+import cardTPL from '../template/cards.hbs';
 
 const select =document.querySelector('.country-searcher');
+const container = document.querySelector('#container');
 
 select.addEventListener('change', onOptionSelect);
 
 function onOptionSelect(evt) {
     // шукаємо id селекту
-    console.log(evt.target.querySelector('option:checked').id);
     const code = evt.target.querySelector('option:checked').id
     
     getEventsByCountry(code).then(
-        // код рендеру на сторінці
+        response => {
+            const data = response.data._embedded.events
+
+            const markup = cardTPL(data)
+            container.innerHTML = markup;
+        }
     )
     
 }
@@ -18,9 +24,9 @@ function onOptionSelect(evt) {
 
 async function getEventsByCountry(countryCode) {
     try{
-        await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${countryCode}&apikey=eXb8ULUoq4HjIKYn2xDLaMMehZFueL04`)
+        return await axios.get(`https://app.ticketmaster.com/discovery/v2/events.json?countryCode=${countryCode}&apikey=eXb8ULUoq4HjIKYn2xDLaMMehZFueL04`)
 
     } catch(error){
-        
+        console.log(error);
     }
 }
