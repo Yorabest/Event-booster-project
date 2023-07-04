82950c6fd1f4000994ee8b9586bd911a78222cc5
Oleh Tsap <olehtsap07@gmail.com>
2023-07-04 19:03:02 +0300

    закінчив логіку кнопки

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -1,6 +1,6 @@
 const refs = {
     switcher: document.querySelector('.js-switch'),
-    switchPage: document.querySelector('.tui-page-btn'),
+    switchPage: document.querySelector('.tui-is-selected'),
 }
 
 
@@ -9,8 +9,10 @@ refs.switcher.addEventListener('change', switchTheme);
 function switchTheme() {
     if (refs.switcher.checked === true) {
         document.body.classList.add("light-theme");
+        document.querySelector('.tui-is-selected').classList.add("switch-page");
       
     } else {
         document.body.classList.remove("light-theme");
+        document.querySelector('.tui-is-selected').classList.remove("switch-page");
     }
 }
