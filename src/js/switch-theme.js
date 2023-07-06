2f7763083c01add507ef96e0c0415368bd6d22cf
Oleh Tsap <olehtsap07@gmail.com>
2023-07-06 18:11:05 +0300

    якісь зміни

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -7,12 +7,13 @@ const refs = {
 refs.switcher.addEventListener('change', switchTheme);
 
 function switchTheme() {
-    if (refs.switcher.checked === true) {
-        document.body.classList.add("light-theme");
-        document.querySelector('.tui-is-selected').classList.add("switch-page");
-      
-    } else {
-        document.body.classList.remove("light-theme");
-        document.querySelector('.tui-is-selected').classList.remove("switch-page");
-    }
+  if (refs.switcher.checked === true) {
+    document.body.classList.add("light-theme");
+    document.querySelector('.tui-is-selected').classList.add("switch-page");
+    localStorage.setItem("theme", "light");
+  } else {
+    document.body.classList.remove("light-theme");
+    document.querySelector('.tui-is-selected').classList.remove("switch-page");
+    localStorage.setItem("theme", "dark");
+  }
 }
