76db859e8ff78f112d52d0d3fd464253c6996cc5
Oleh Tsap <olehtsap07@gmail.com>
2023-07-06 18:22:43 +0300

    деякі зміни

--- src/js/switch-theme.js
+++ src/js/switch-theme.js
@@ -3,17 +3,16 @@ const refs = {
     switchPage: document.querySelector('.tui-is-selected'),
 }
 
-
 refs.switcher.addEventListener('change', switchTheme);
 
 function switchTheme() {
   if (refs.switcher.checked === true) {
-    document.body.classList.add("light-theme");
+    document.body.classList.add('light-theme');
     document.querySelector('.tui-is-selected').classList.add("switch-page");
-    localStorage.setItem("theme", "light");
+    localStorage.setItem('theme', 'light');
   } else {
-    document.body.classList.remove("light-theme");
+    document.body.classList.remove('light-theme');
     document.querySelector('.tui-is-selected').classList.remove("switch-page");
-    localStorage.setItem("theme", "dark");
+    localStorage.setItem('theme', 'dark');
   }
 }
